d0bd8b478de1f8ce7d9fc1906011535a4183cf1d
cwosw <182838250+cwosw@users.noreply.github.com>
2025-02-15 17:51:06 -0800

    minor fixes

--- onboot.sh
+++ onboot.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 xmutil unloadapp
 xmutil loadapp kr260-bist
-# set up bridge, br0 shoudl exist
+nmcli con del bridge-br0
+# set up bridge, br0 should exist
 for i in {1..5}; do
-    nmcli con del 'Wired connection ${i}' || true 1>&2 >> /dev/null
+    nmcli con del Wired\ connection\ $i || true 1>&2 >> /dev/null
 done
 for i in {0..4}; do
     nmcli con add type ethernet slave-type bridge con-name bridge-br0 ifname eth$i master br0
