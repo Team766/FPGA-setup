e125c516e7f603fc95390a269d6daff5b48f0536
cwosw <182838250+cwosw@users.noreply.github.com>
2025-02-15 17:33:47 -0800

    wrote service files

--- install.sh
+++ install.sh
@@ -1,2 +1,22 @@
 #! /bin/bash
-
+# root check
+if [ $EUID -ne 0 ]; then
+    echo "Please run this with root"
+    exit 1
+fi
+# check for onboot in CWD
+if [ ! -f onboot.sh ]; then
+    echo "run in the FPGA repo dir"
+    exit 2
+fi
+# go for it
+cp onboot.sh /
+# modify perms on /onboot.sh
+chmod 744 /onboot.sh
+# copy and configure system file
+cp FPGAonBoot.service /etc/systemd/system
+chmod 755 /etc/systemd/system/FPGAonBoot.service
+systemctl daemon-reload
+# set up ethernet bridge and start everything
+nmcli conn add type bridge con-name br0 ifname br0
+systemctl enable --now FPGAonBoot.service
--- onboot.sh
+++ onboot.sh
@@ -1,2 +1,10 @@
 #! /bin/bash
-
+xmutil unloadapp
+xmutil loadapp kr260-bist
+# set up bridge, br0 shoudl exist
+for i in {1..5}; do
+    nmcli con del 'Wired connection ${i}' || true 1>&2 >> /dev/null
+done
+for i in {0..4}; do
+    nmcli con add type ethernet slave-type bridge con-name bridge-br0 ifname eth$i master br0
+done
